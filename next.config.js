720a9e8ea4d4debd81040a38c64826af4faa6103
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-12 21:05:19 +0200

    Fix bug

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'my-vastra.herokuapp.com', , 'placeimg.com'],
+    domains: ['localhost', 'my-vastra.herokuapp.com', 'placeimg.com'],
   },
 };
 
